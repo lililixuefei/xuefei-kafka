1cbb2df5aaf5714e9f7c73e939f3b514cc6722c5
lixuefei06 <13273726905@163.com>
2023-06-12 22:44:34 +0800

    消费者客户端示例

--- kafka-producer-consumer/src/main/java/com/xuefei/kafka/consumer/KafkaConsumerAnalysis.java
+++ kafka-producer-consumer/src/main/java/com/xuefei/kafka/consumer/KafkaConsumerAnalysis.java
@@ -1,6 +1,7 @@
 package com.xuefei.kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -23,15 +24,27 @@ public class KafkaConsumerAnalysis {
 	public static final String groupId = "group.demo";
 	public static final AtomicBoolean isRunning = new AtomicBoolean(true);
 
-	public static Properties initConfig() {
+//	public static Properties initConfig() {
+//		Properties props = new Properties();
+//		props.put("key.deserializer",
+//				"org.apache.kafka.common.serialization.StringDeserializer");
+//		props.put("value.deserializer",
+//				"org.apache.kafka.common.serialization.StringDeserializer");
+//		props.put("bootstrap.servers", brokerList);
+//		props.put("group.id", groupId);
+//		props.put("client.id", "consumer.client.id.demo");
+//		return props;
+//	}
+
+	public static Properties initConfig(){
 		Properties props = new Properties();
-		props.put("key.deserializer",
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
 				"org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer",
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
 				"org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("bootstrap.servers", brokerList);
-		props.put("group.id", groupId);
-		props.put("client.id", "consumer.client.id.demo");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		props.put(ConsumerConfig.CLIENT_ID_CONFIG, "client.id.demo");
 		return props;
 	}
 
